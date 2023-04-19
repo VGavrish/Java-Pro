369295bec2d665c51379fabc2bd39d26bc5d4ec8
Гавріш Віталій Георгійович <VGavrish@sensebank.com.ua>
2023-04-19 11:18:27 +0300

    HomeWork for OOP. Create new classes Car and Employee and two identity classes

---
+++ src/main/java/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    public void startElectricity () {
+
+    }
+
+    public void startCommand() {
+
+    }
+
+    public void startFuelSystem() {
+
+    }
+
+
+}
---
+++ src/main/java/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private int phone;
+    private byte year;
+
+    public Employee ( String fullName, String position, String email, int phone, byte year) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.year = year;
+    }
+}
